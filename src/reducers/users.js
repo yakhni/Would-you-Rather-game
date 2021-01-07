d9374d54f185a0e83025461f946844f308f8a7b4
Farhan Ul Haq <farhan@openvpn.net>
2021-01-07 08:31:49 -0800

    fix: leaderboard not updated when creating new question. solution: when new question created, also add question ID to redux's users.questions

--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,5 +1,6 @@
 import { SET_USERS } from '../actions/users'
 import { SAVE_USER_ANSWER } from '../actions/users'
+import { NEW_QUESTION } from '../actions/questions'
 
 export default function users(state = {}, action) {
   switch (action.type) {
@@ -19,6 +20,15 @@ export default function users(state = {}, action) {
           answers: {
             ...newAnswers
           }
+        }
+      }
+    case NEW_QUESTION:
+      const { question } = action
+      return {
+        ...state,
+        [question.author] : {
+          ...state[question.author],
+          questions: state[question.author].questions.concat([question.id])
         }
       }
     default:
