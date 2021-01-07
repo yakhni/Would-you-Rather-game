db0eca80be11779d11135b402dfb2bc8f29d19e8
Farhan Ul Haq <farhan@openvpn.net>
2021-01-07 13:57:42 -0800

    refactor: Make Votes into a functional component

--- src/components/Votes.js
+++ src/components/Votes.js
@@ -1,27 +1,22 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Card from 'react-bootstrap/Card'
 import ProgressBar from 'react-bootstrap/ProgressBar'
 
-class Votes extends Component {
-  render() {
-    const { answer, percentVote, votes, totalVotes, border } = this.props
-    return (
+const Votes = ({ answer, percentVote, votes, totalVotes, border }) => (
+  <Card.Body>
+    <Card border={border}>
       <Card.Body>
-        <Card border={border}>
-          <Card.Body>
-            <p>You'd rather {answer}</p>
-            <ProgressBar
-              variant='info'
-              className="progress"
-              striped label={`${percentVote}%`}
-              now={percentVote}
-            />
-            <p className='text-center'><small>{votes} out of {totalVotes} votes</small></p>
-          </Card.Body>
-        </Card>
+        <p>You'd rather {answer}</p>
+        <ProgressBar
+          variant='info'
+          className="progress"
+          striped label={`${percentVote}%`}
+          now={percentVote}
+        />
+        <p className='text-center'><small>{votes} out of {totalVotes} votes</small></p>
       </Card.Body>
-    )
-  }
-}
+    </Card>
+  </Card.Body>
+)
 
 export default Votes
